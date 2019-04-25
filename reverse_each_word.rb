804be6d75859116488deb92ddfe6f116a0dab1ef
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 17:07:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
+<<<<<<< HEAD
   reversed_words = ""
   string_array = string.split(" ")
   string_array.collect do |i| 
@@ -6,5 +7,13 @@ def reverse_each_word(string)
   
   end
   return reversed_words[1, (reversed_words.length-1)]
+=======
+  reversed_words
+  string_array = string.split(" ")
+  string_array.collect do |i| 
+   reversed_words = "#{reversed_words} #{i.to_s.reverse}"
+  end
+  return reversed_words
+>>>>>>> fe265fa9e41bb02650494d4dffba8d400b424fae
 end
 
